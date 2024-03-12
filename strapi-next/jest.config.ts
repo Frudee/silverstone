d5da6ed803f0bf4f3b4eeeb8f0e763093c10cf9a
frudee <dsukhorukov16@gmail.com>
2024-03-12 20:54:15 +0300

    change coverage directories

--- strapi-next/jest.config.ts
+++ strapi-next/jest.config.ts
@@ -28,7 +28,11 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "components/**/*.{js,jsx,ts,tsx}",
+    "pages/**/*.{js,jsx,ts,tsx}",
+    "utils/**/*.{js,ts}",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
