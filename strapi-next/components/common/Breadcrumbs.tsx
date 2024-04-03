2a044fa5b798c98133d6b8e72bc415357cfc3ad7
Сухоруков Дмитрий Дмитриевич <dsuhorukov@magnezit.com>
2024-04-03 11:39:19 +0500

    refactor into custom hook

--- strapi-next/components/common/Breadcrumbs.tsx
+++ strapi-next/components/common/Breadcrumbs.tsx
@@ -1,58 +1,13 @@
-import { useRouter } from "next/router";
 import BreadcrumbItem from "./BreadcrumbItem";
-import { useEffect, useState } from "react";
-import { getCategoryBySlug } from "../../graphql/queries/category/getCategoryBySlug";
-import { getProductBySlug } from "../../graphql/queries/product/getProductBySlug";
-import Category from "../../types/category";
-import Product from "../../types/product";
-import pageMappings from "../../utils/pageMappings";
+import { useBreadcrumbs } from "../../utils/hooks";
 
-interface Breadcrumb {
+export interface Breadcrumb {
   text: string;
   href: string;
 }
 
 const Breadcrumbs: React.FC = () => {
-  const router = useRouter();
-  const [breadcrumbs, setBreadcrumbs] = useState<Breadcrumb[]>([]);
-
-  useEffect(() => {
-    const pathSegments = router.asPath
-      .split("/")
-      .filter((segment) => segment !== "");
-    const promises: Promise<Category | Product>[] = [];
-
-    pathSegments.forEach((segment, index) => {
-      if (index > 0 && pathSegments[index - 1] === "catalogue") {
-        promises.push(getCategoryBySlug(segment));
-      }
-      if (index > 0 && pathSegments[index - 2] === "catalogue") {
-        promises.push(getProductBySlug(segment));
-      }
-    });
-    pathSegments.length === 3 && pathSegments.splice(-2);
-    pathSegments.length === 2 && pathSegments.splice(-1);
-
-    const resultArray: Breadcrumb[] = pathSegments.map((segment, index) => {
-      const previousSegment = index > 0 ? pathSegments[index - 1] : "";
-      const href = `/${segment}${
-        previousSegment !== "" ? "/" + previousSegment : ""
-      }`;
-      return { text: pageMappings[segment] || segment, href: href };
-    });
-
-    Promise.all(promises).then((data) => {
-      data.forEach((segment) => {
-        const previousHref = resultArray[resultArray.length - 1]?.href;
-        resultArray.push({
-          text: segment?.attributes?.name,
-          href: `${previousHref}/${segment?.attributes?.slug}`,
-        });
-      });
-      setBreadcrumbs([{ text: "Главная", href: "/" }, ...resultArray]);
-    });
-  }, [router.asPath]);
-
+  const breadcrumbs = useBreadcrumbs();
   if (breadcrumbs.length <= 1) {
     return null;
   }
--- strapi-next/utils/hooks.ts
+++ strapi-next/utils/hooks.ts
@@ -1,4 +1,11 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Breadcrumb } from "../components/common/Breadcrumbs";
+import Product from "../types/product";
+import Category from "../types/category";
+import { getCategoryBySlug } from "../graphql/queries/category/getCategoryBySlug";
+import { getProductBySlug } from "../graphql/queries/product/getProductBySlug";
+import pageMappings from "./pageMappings";
 
 export const useAutoCloseMenu = (): [
   boolean,
@@ -24,4 +31,48 @@ export const useAutoCloseMenu = (): [
   }, [isOpen]);
 
   return [isOpen, setIsOpen];
+};
+
+export const useBreadcrumbs = () => {
+  const router = useRouter();
+  const [breadcrumbs, setBreadcrumbs] = useState<Breadcrumb[]>([]);
+
+  useEffect(() => {
+    const pathSegments = router.asPath
+      .split("/")
+      .filter((segment) => segment !== "");
+    const promises: Promise<Category | Product>[] = [];
+
+    pathSegments.forEach((segment, index) => {
+      if (index > 0 && pathSegments[index - 1] === "catalogue") {
+        promises.push(getCategoryBySlug(segment));
+      }
+      if (index > 0 && pathSegments[index - 2] === "catalogue") {
+        promises.push(getProductBySlug(segment));
+      }
+    });
+    pathSegments.length === 3 && pathSegments.splice(-2);
+    pathSegments.length === 2 && pathSegments.splice(-1);
+
+    const resultArray: Breadcrumb[] = pathSegments.map((segment, index) => {
+      const previousSegment = index > 0 ? pathSegments[index - 1] : "";
+      const href = `/${segment}${
+        previousSegment !== "" ? "/" + previousSegment : ""
+      }`;
+      return { text: pageMappings[segment] || segment, href: href };
+    });
+
+    Promise.all(promises).then((data) => {
+      data.forEach((segment) => {
+        const previousHref = resultArray[resultArray.length - 1]?.href;
+        resultArray.push({
+          text: segment?.attributes?.name,
+          href: `${previousHref}/${segment?.attributes?.slug}`,
+        });
+      });
+      setBreadcrumbs([{ text: "Главная", href: "/" }, ...resultArray]);
+    });
+  }, [router.asPath]);
+
+  return breadcrumbs;
 };
